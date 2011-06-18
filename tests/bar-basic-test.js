d50bff7f15eea5533a79de834a58e6aa66e86281
Fedor Indutny <fedor.indutny@gmail.com>
2011-06-18 14:38:25 +0700

    (initial)

---
+++ tests/bar-basic-test.js
@@ -0,0 +1,15 @@
+var vows = require('vows'),
+    assert = require('assert');
+
+var bar = require('../lib/bar');
+
+vows.describe('Bar').addBatch({
+  'Calling bar.create()': {
+    topic: function() {
+      bar.create(__dirname + '/data');
+    },
+    'should create bar.Bar instance': function(I) {
+      assert.instanceOf(I, bar.Bar);
+    }
+  }
+}).export(module);
